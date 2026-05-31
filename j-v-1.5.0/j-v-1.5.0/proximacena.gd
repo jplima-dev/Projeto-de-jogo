e12c8a40208f4deb2a33b9e337e615d6ab93673a
Juan Pablo <jp.lima.tech@gmail.com>
2026-05-31 17:20:16 -0300

    sai da sala luz acende

--- j-v-1.5.0/j-v-1.5.0/proximacena.gd
+++ j-v-1.5.0/j-v-1.5.0/proximacena.gd
@@ -6,13 +6,17 @@ var can_tp = true
 
 
 func _on_body_entered(body: Node2D) -> void:
-	
+
 	if body.is_in_group("player") and can_tp and destino:
-		
+
 		can_tp = false
-		
+
+		var fade = get_tree().current_scene.get_node_or_null("CanvasLayer/Fade")
+		if fade:
+			fade.hide()
+
 		body.global_position = destino.global_position
-		
+
 		await get_tree().create_timer(0.5).timeout
-		
+
 		can_tp = true
