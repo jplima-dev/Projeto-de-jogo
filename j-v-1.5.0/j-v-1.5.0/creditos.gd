c98599ae5fccaca36dd331cc4aa7f1ddfa36b044
Juan Pablo <jp.lima.tech@gmail.com>
2026-06-05 18:15:56 -0300

    tela super melhorada

--- j-v-1.5.0/j-v-1.5.0/creditos.gd
+++ j-v-1.5.0/j-v-1.5.0/creditos.gd
@@ -1,9 +1,11 @@
 extends Control
 
 @onready var scroll_container = $ScrollContainer
+@onready var anim = $AnimationPlayer
 
-var velocidade = 60.0
-
+var velocidade = 100.0
+var terminou = false
+var ultimo_scroll = -1
 
 func _ready():
 	scroll_container.scroll_vertical = 0
@@ -16,6 +18,17 @@ func _ready():
 
 
 func _physics_process(delta):
-	scroll_container.set_v_scroll(
-		scroll_container.get_v_scroll() + velocidade * delta
-	)
+	if terminou:
+		return
+
+	scroll_container.scroll_vertical += velocidade * delta
+
+	# Se o scroll não mudou desde o frame anterior,
+	# significa que chegou ao fim.
+	if scroll_container.scroll_vertical == ultimo_scroll:
+		terminou = true
+		print("CHEGOU NO FINAL")
+		anim.play("fade_logo_creditos")
+	
+
+	ultimo_scroll = scroll_container.scroll_vertical	
